919b1b5b610e228ddd8bf0f145225a6563b39da5
Ilya Kochik <ilya.kochik@gmail.com>
2020-05-11 21:48:21 +0100

    Refactoring to enable quick set-up environment and saving instances of trained objects

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from pypokerengine.api.game import setup_config, start_poker
-from relepo.relepo_player_v0 import relepo_player_v0
-from relepo.relepo_player_v1 import relepo_player_v1
+from relepo.player_v0 import Player_v0
+from relepo.player_v1 import Player_v1
 from pprint import pprint
+import numpy as np
+import pandas as pd
 import pickle
 
 CONFIG = {
@@ -16,49 +18,70 @@ CONFIG = {
 }
 
 
-def calc_score(game_result, eval_player):
-    stacks = {i['name']: i['stack'] for i in game_result['players']}
-    return stacks[eval_player] / sum(stacks.values())
+def calc_score(game_result):
+    total_stacks = sum([i['stack'] for i in game_result['players']])
+    scores = {i['name']: i['stack'] / total_stacks for i in game_result['players']}
+
+    return scores
 
 
-def train(config, eval_player='p1', num_episodes=1000):
-    scores = []
-    for i_episode in range(num_episodes):
-        print('\rEpisode {}/{}.'.format(i_episode + 1, num_episodes), end='')
+def train(config, num_episodes=1000):
+    player_names = [i['name'] for i in config.players_info]
+    scores = pd.DataFrame(columns=player_names)
 
+    for i_episode in range(1, num_episodes + 1):
         game_result = start_poker(config, verbose=0)
+        scores = pd.DataFrame.append(scores, calc_score(game_result), ignore_index=True)
 
-        scores.append(calc_score(game_result, eval_player))
+        if i_episode % 100 == 0:
+            print('\rTraining episode {}/{}, scores are {}.'.
+                  format(i_episode, num_episodes, scores.tail(100).mean().round(2).to_dict()), end='')
+
+    print()
+    print('Players performance:')
+    print(scores.mean())
     print()
 
-    return sum(scores) / len(scores)
+
+def env_create(config):
+    print("Loading environment")
+    pypoker_config = setup_config(**config['env'])
+
+    for k, v in enumerate(config['players']):
+        p_name, p_class, p_load, p_save, p_show, p_params = v
+        if p_load:
+            player = pickle.load(open(p_load, 'rb'))
+        else:
+            player = globals()[p_class](**p_params)
+
+        if p_show:
+            print(player)
+
+        config['players'][k][1] = player
+        pypoker_config.register_player(name=p_name, algorithm=player)
+
+    print()
+    return pypoker_config
 
 
-Q = None
-if Q_LOAD:
-    Q = pickle.load(open(Q_FILE, 'rb'))
-    if isinstance(Q, dict): print("Q loaded (size: {})".format(len(Q)))
+def env_save(config):
+    for p_name, p_instance, p_load, p_save, p_show, p_params in config['players']:
+        if p_save:
+            print('{} saved to {}'.format(p_name, p_save))
+            pickle.dump(p_instance, open(p_save, 'wb'))
 
-Q_sorted = {}
-for k1, v1 in Q.items():
-    for k2, v2 in v1.items():
-        Q_sorted[(k1, k2)] = v2
 
-pprint(sorted(Q_sorted.items(), key=lambda v: v[1]))
+if __name__ == '__main__':
+    env_config = env_create(CONFIG)
 
-eval_player = relepo_player_v1(Q)
-config = setup_config(max_round=100, initial_stack=100, small_blind_amount=5)
-config.register_player(name='p1', algorithm=eval_player)
-config.register_player(name='p2', algorithm=relepo_player_v0())
+    if CONFIG['train']:
+        train(env_config, num_episodes=CONFIG['train'])
 
-if TRAIN:
-    performance = train(config, eval_player='p1', num_episodes=1000)
-    print("Player performance is {}".format(performance))
+    if CONFIG['demo']:
+        for p_name, p_instance, p_load, p_save, p_show, p_params in CONFIG['players']:
+            if 'verbose' in p_instance.__dict__.keys(): p_instance.verbose = 1
 
-if DEMO:
-    eval_player.verbose = 1
-    game_result = start_poker(config, verbose=1)
+        for i in range(CONFIG['demo']):
+            start_poker(env_config, verbose=1)
 
-if Q_SAVE:
-    pickle.dump(eval_player.Q, open(Q_FILE, 'wb'))
-    print("Q saved (size: {})".format(len(eval_player.Q)))
+    env_save(CONFIG)
